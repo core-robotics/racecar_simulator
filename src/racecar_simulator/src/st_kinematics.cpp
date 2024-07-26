b37e3c2652d95c4f0d792044602970122c6fb14a
DoHyun_Kwon <tlstoddk99@gmail.com>
2024-07-26 17:25:27 +0900

    0726_bag

--- src/racecar_simulator/src/st_kinematics.cpp
+++ src/racecar_simulator/src/st_kinematics.cpp
@@ -147,13 +147,13 @@ CarState STKinematics::update_with_pacejka(const CarState start, double accel, d
         std::random_device rd;
         std::mt19937 gen(rd());
         std::normal_distribution<> d(0, 1);
-        end.x = start.x + x_dot * dt + d(gen) * 0.01;
-        end.y = start.y + y_dot * dt + d(gen) * 0.01;
-        end.theta = start.theta + theta_dot * dt + d(gen) * 0.01;
-        end.velocity = start.velocity + v_dot * dt + d(gen) * 0.01;
-        end.steer_angle = start.steer_angle + steer_angle_dot * dt + d(gen) * 0.01;
-        end.angular_velocity = start.angular_velocity + theta_double_dot * dt + d(gen) * 0.01;
-        end.slip_angle = start.slip_angle + slip_angle_dot * dt + d(gen) * 0.01;
+        end.x = start.x + x_dot * dt + d(gen) * 0.0;
+        end.y = start.y + y_dot * dt + d(gen) * 0.0;
+        end.theta = start.theta + theta_dot * dt + d(gen) * 0.0;
+        end.velocity = start.velocity + v_dot * dt + d(gen) * 0.05;
+        end.steer_angle = start.steer_angle + steer_angle_dot * dt + d(gen) * 0.0;
+        end.angular_velocity = start.angular_velocity + theta_double_dot * dt + d(gen) * 0.05;
+        end.slip_angle = start.slip_angle + slip_angle_dot * dt + d(gen) * 0.0;
         if (end.theta > M_PI)
             end.theta -= 2 * M_PI;
         else if (end.theta < -M_PI)
