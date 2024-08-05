b9f22c8131e454295c78992d3451ed3e9ecb413c
DoHyun_Kwon <tlstoddk99@gmail.com>
2024-08-05 20:54:57 +0900

    rviz render error

--- src/racecar_simulator/launch/simulator.launch.py
+++ src/racecar_simulator/launch/simulator.launch.py
@@ -2,127 +2,121 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command
 from launch_ros.parameter_descriptions import ParameterValue
-from launch.event_handlers import OnProcessStart
-from launch.actions import TimerAction, RegisterEventHandler, ExecuteProcess
-from launch.substitutions import FindExecutable
+# from launch.event_handlers import OnProcessStart
+# from launch.actions import TimerAction, RegisterEventHandler, ExecuteProcess
+# from launch.substitutions import FindExecutable
 import os
 
+
 def generate_launch_description():
-    
+
     ld = LaunchDescription()
-    
+
     # Get the share directory for the racecar_simulator package
     pkg_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
     # Define the path to the map server parameters file
-    map_server_params = os.path.join(pkg_dir, 'maps', 'c_track.yaml')
-    
+    map_yaml = os.path.join(pkg_dir, "maps", "c_track.yaml")
+    map_pgm = os.path.join(pkg_dir, "maps", "c_track.pgm")
+
     # Define the path to the rviz configuration file
-    rviz_config_file = os.path.join(pkg_dir, 'params', 'simulator.rviz')
-    
+    rviz_config_file = os.path.join(pkg_dir, "params", "simulator.rviz")
+
     # # Define the path to the robot description file
-    car0_xacro_file = os.path.join(pkg_dir, 'params', 'racecar0.xacro')
-    car1_xacro_file = os.path.join(pkg_dir, 'params', 'racecar1.xacro')
+    car0_xacro_file = os.path.join(pkg_dir, "params", "racecar0.xacro")
+    # car1_xacro_file = os.path.join(pkg_dir, "params", "racecar1.xacro")
     
-    car0_desc=Command([
-        'xacro',
-        car0_xacro_file,
-        'prefix:=0',
-    ])
-    
-    # car1_desc=Command([
-    #     'xacro',
-    #     car1_xacro_file,
-    #     'prefix:=1',
-    # ])
-    
+    # with open(car0_urdf_file, "r") as infp:
+    #     car0_urdf = infp.read()
+    # with open(car1_urdf_file, "r") as infp:
+    #     car1_urdf = infp.read()
+
+    # robot0_state_publisher_node = Node(
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     name="robot_state_publisher",
+    #     namespace="racecar0",
+    #     output="screen",
+    #     parameters=[
+    #         {
+    #             "use_sim_time": True,
+    #             "robot_description": car0_urdf,
+    #         }
+    #     ],
+    # )
+
+    # robot1_state_publisher_node = Node(
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     name="robot_state_publisher",
+    #     namespace="racecar1",
+    #     output="screen",
+    #     parameters=[
+    #         {
+    #             "use_sim_time": True,
+    #             "robot_description": car1_urdf,
+    #         }
+    #     ],
+    # )
     
     robot0_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='robot_state_publisher',
-        namespace='racecar0',
-        output='screen',
-        parameters=[{'robot_description':ParameterValue(Command(['xacro ', car0_xacro_file, ' prefix:=0']), value_type=str)}],
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        name="robot_state_publisher",
+        namespace="racecar0",
+        output="screen",
+        parameters=[
+            {
+                "robot_description": ParameterValue(
+                    Command(["xacro ", str(car0_xacro_file), " prefix:=0"]), value_type=str
+                )
+            }
+        ],
     )
-    
-    # robot2_state_publisher_node = Node(
-    #     package='robot_state_publisher',
-    #     executable='robot_state_publisher',
-    #     name='robot_state_publisher',
-    #     namespace='racecar1',
-    #     output='screen',
-    #     parameters=[{'robot_description': car1_desc}]
-    # )
-    
 
-    
-    racecar_node =Node(
-        package='racecar_simulator',
-        executable='simulator',
-        name='racecar_simulator',
-        output='screen',
+    # robot1_state_publisher_node = Node(
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     name="robot_state_publisher",
+    #     namespace="racecar1",
+    #     output="screen",
+    #     parameters=[
+    #         {
+    #             "robot_description": ParameterValue(
+    #                 Command(["xacro ", str(car1_xacro_file), " prefix:=1"]), value_type=str
+    #             )
+    #         }
+    #     ],
+    # )
+
+    racecar_node = Node(
+        package="racecar_simulator",
+        executable="simulator",
+        name="racecar_simulator",
+        output="screen",
     )
-    
-    lifecycle_manager_node = Node(
-        package='nav2_lifecycle_manager',
-        executable='lifecycle_manager',
-        name='lifecycle_manager',
-        output='screen',
-        parameters=[{
-            # 'use_sim_time': True, # Set to True if using simulated time
-            'autostart': True, # Automatically startup the managed nodes
-            'node_names': ['map_server'], # Specify the lifecycle nodes to be managed
-        }],
+
+    map_publisher_node = Node(
+        package="racecar_simulator",
+        executable="map_publisher",
+        name="map_publisher",
+        output="screen",
+        parameters=[{"yaml_file_path": map_yaml, "pgm_file_path": map_pgm}],
     )
-    
+
     rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', rviz_config_file],
-        output='screen',
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_config_file],
+        output="screen",
     )
-    
-    map_server_node = Node(
-        package='nav2_map_server',
-        executable='map_server',
-        name='map_server',
-        output='screen',
-        parameters=[{'yaml_filename': map_server_params}],
-    )
-    
-    delay_node = TimerAction(
-        period=1.0,
-        actions=[
-            map_server_node
-            ,racecar_node
-            ,lifecycle_manager_node
-             ,robot0_state_publisher_node
-            # ,robot2_state_publisher_node
-            ]
-    )
-    
-    node_start=RegisterEventHandler(
-        event_handler=OnProcessStart(
-            target_action=rviz_node,
-            on_start=[delay_node]
-        )
-    )
+
     ld.add_action(rviz_node)
-    ld.add_action(node_start)
-    # ld.add_action(
-    # ExecuteProcess(
-    #     cmd=[[
-    #         FindExecutable(name='ros2'),
-    #         " service call",
-    #         " /map_server/load_map",
-    #         " nav2_msgs/srv/LoadMap",
-    #         ' "{map_url: /home/a/racecar_simulator/src/racecar_simulator/maps/c_track.yaml}"',
-    #         " -r 10"
-    #     ]],
-    #     shell=True
-    # )
-    # )
+    ld.add_action(map_publisher_node)
+    ld.add_action(racecar_node)
+    ld.add_action(robot0_state_publisher_node)
+    # ld.add_action(robot1_state_publisher_node)
+    
 
     return ld
--- src/racecar_simulator/src/simulator.cpp
+++ src/racecar_simulator/src/simulator.cpp
@@ -1,9 +1,13 @@
 #include <chrono>
 #include <functional>
 #include <memory>
-#include "geometry_msgs/msg/transform_stamped.hpp"
-#include "rclcpp/rclcpp.hpp"
-#include "tf2_ros/transform_broadcaster.h"
+#include <rclcpp/rclcpp.hpp>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/static_transform_broadcaster.h>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+
+
 
 using namespace std::chrono_literals;
 
@@ -11,37 +15,174 @@ class Racecar_simulator : public rclcpp::Node
 {
 public:
   Racecar_simulator()
-  : Node("Racecar_simulator")
+      : Node("Racecar_simulator")
   {
     tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(this);
-    timer_ = this->create_wall_timer(
-      100ms, std::bind(&Racecar_simulator::broadcast_timer_callback, this));
+    simulator_timer = this->create_wall_timer(
+        0.01ms, std::bind(&Racecar_simulator::simulator_loop, this));
+    pub_timer= this->create_wall_timer(
+        10ms, std::bind(&Racecar_simulator::pub_loop, this));
   }
 
 private:
-  void broadcast_timer_callback()
+  void simulator_loop()
+  {
+    tf_map_baselink_0();
+    tf_wheel_l_0();
+    tf_wheel_r_0();
+    // tf_map_baselink_1();
+    // tf_wheel_l_1();
+    // tf_wheel_r_1();
+  }
+
+  void pub_loop()
+  {
+  }
+
+  void tf_map_baselink_0()
   {
     geometry_msgs::msg::TransformStamped t;
 
     t.header.stamp = this->get_clock()->now();
     t.header.frame_id = "map";
     t.child_frame_id = "base_link0";
-    t.transform.translation.x = 1.0;
-    t.transform.translation.y = 1.0;
-    t.transform.translation.z = 0.0;
-    t.transform.rotation.x = 0.0;
-    t.transform.rotation.y = 0.0;
-    t.transform.rotation.z = 0.0;
-    t.transform.rotation.w = 1.0;
 
+    t.transform.translation.x = 1.0;
+    t.transform.translation.y = -5.0;
+    t.transform.translation.z = 0.0;
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, 0);
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    // Send the transformation
     tf_broadcaster_->sendTransform(t);
   }
 
-rclcpp::TimerBase::SharedPtr timer_;
+  void tf_wheel_l_0()
+  {
+    geometry_msgs::msg::TransformStamped t;
+
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = "front_left_hinge0";
+    t.child_frame_id = "front_left_wheel0";
+
+    t.transform.translation.x = 0.0;
+    t.transform.translation.y = 0.0;
+    t.transform.translation.z = 0.0;
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, 0.4);
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    // Send the transformation
+    tf_broadcaster_->sendTransform(t);
+  }
+
+  void tf_wheel_r_0()
+  {
+    geometry_msgs::msg::TransformStamped t;
+
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = "front_right_hinge0";
+    t.child_frame_id = "front_right_wheel0";
+
+    t.transform.translation.x = 0.0;
+    t.transform.translation.y = 0.0;
+    t.transform.translation.z = 0.0;
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, 0.3);
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    // Send the transformation
+    tf_broadcaster_->sendTransform(t);
+  }
+
+  void tf_map_baselink_1()
+  {
+    geometry_msgs::msg::TransformStamped t;
+
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = "map";
+    t.child_frame_id = "base_link1";
+
+    t.transform.translation.x = -2.0;
+    t.transform.translation.y = -5.0;
+    t.transform.translation.z = 0.0;
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, 0);
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    // Send the transformation
+    tf_broadcaster_->sendTransform(t);
+  }
+
+  void tf_wheel_l_1()
+  {
+    geometry_msgs::msg::TransformStamped t;
+
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = "front_left_hinge1";
+    t.child_frame_id = "front_left_wheel1";
+
+    t.transform.translation.x = 0.0;
+    t.transform.translation.y = 0.0;
+    t.transform.translation.z = 0.0;
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, 0.4);
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    // Send the transformation
+    tf_broadcaster_->sendTransform(t);
+  }
+
+  void tf_wheel_r_1()
+  {
+    geometry_msgs::msg::TransformStamped t;
+
+    t.header.stamp = this->get_clock()->now();
+    t.header.frame_id = "front_right_hinge1";
+    t.child_frame_id = "front_right_wheel1";
+
+    t.transform.translation.x = 0.0;
+    t.transform.translation.y = 0.0;
+    t.transform.translation.z = 0.0;
+
+    tf2::Quaternion q;
+    q.setRPY(0, 0, 0.3);
+    t.transform.rotation.x = q.x();
+    t.transform.rotation.y = q.y();
+    t.transform.rotation.z = q.z();
+    t.transform.rotation.w = q.w();
+
+    // Send the transformation
+    tf_broadcaster_->sendTransform(t);
+  }
+
+  rclcpp::TimerBase::SharedPtr simulator_timer;
+  rclcpp::TimerBase::SharedPtr pub_timer;
   std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<Racecar_simulator>());
