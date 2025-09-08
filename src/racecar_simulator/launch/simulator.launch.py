ca245ba1172fb263e39fcf1fbf7227f4a03f11ea
DoHyun_Kwon <tlstoddk99@gmail.com>
2025-09-08 15:17:12 +0900

    temp

--- src/racecar_simulator/launch/simulator.launch.py
+++ src/racecar_simulator/launch/simulator.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions import Command
 from launch_ros.parameter_descriptions import ParameterValue
+from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
